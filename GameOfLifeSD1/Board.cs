5c421335230cbb7f9bf4122d9ac246106547de99
Cody Norwood <CodyBonez@yahoo.com>
2026-02-05 11:23:09 -0800

    Add project files.

---
+++ GameOfLifeSD1/Board.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLifeSD1
+{
+    public class Board
+    {
+        // Create 2d array of bools
+   
+        
+        public void DisplayBoard()
+
+
+        {
+            bool[,] board = new bool[100, 100];
+            int i = 0;
+            int j = 0;
+          
+            for (i = 0; i < 100; i++)
+            {
+      
+                for (j = 0; j < 100; j++)
+                {
+                    Console.Write($"[{board[j, i]}]");
+            
+                }
+            }
+            
+      
+        }
+       public void ClearBoard()
+        {
+            bool[,] board = new bool[100, 100];
+            int i = 0;
+            int j = 0;
+            for (i = 0; i < 100; i++)
+            {
+           
+                for (j = 0; j < 100; j++)
+                {
+                    Console.Write($"[]");
+
+                }
+            }
+
+        }
+       
+
+    }
+}
---
+++ GameOfLifeSD1/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace GameOfLifeSD1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string choice;
+            Board board = new Board();
+            do
+            {
+                Console.WriteLine("\n\n1. Display 2. Clear");
+                choice = Console.ReadLine();
+                switch (choice)
+                {
+                    case "1":
+                        board.DisplayBoard();
+                        break;
+                    case "2":
+                        board.ClearBoard();
+                        break;
+                }
+            } while (choice != "1" || choice != "2");
+
+
+
+
+        }
+    }
+}
